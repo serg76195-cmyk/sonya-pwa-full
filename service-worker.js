1070c27564324e911ce2b7743813aeefb0159eca
serg76195-cmyk <serg76195@gmail.com>
2025-08-16 08:14:09 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,41 @@
+// service-worker.js
+
+const CACHE_NAME = "sonya-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Установка SW и кеширование файлов
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("sonya-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js",
-        "/manifest.json",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
+// Активация и очистка старого кеша
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Обработка запросов
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
